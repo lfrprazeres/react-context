9437fda499d4b38f72e37e6f758a7a1f56fcdcaf
Luiz <luiz.ribeiro@hotmart.com>
2021-07-23 17:37:56 -0300

    login disabled

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -52,7 +52,7 @@ function Login() {
       <Button
         variant="contained"
         color="primary"
-        disabled={'nome'.length < 4}
+        disabled={nome.length < 4}
         onClick={() => history.push('/feira')}
       >
         Avançar
