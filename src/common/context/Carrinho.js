38ebb5818250331aef8e63f64b3c9c8ae7ebd131
Luiz Fernando <lfrprazeres@gmail.com>
2021-09-04 18:33:40 -0300

    aula4.3 - Contando quantidade de produtos

--- src/common/context/Carrinho.js
+++ src/common/context/Carrinho.js
@@ -1,19 +1,32 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = "Carrinho";
 
 export const CarrinhoProvider = ({ children }) => {
   const [carrinho, setCarrinho] = useState([]);
+  const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
   return (
-    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        setCarrinho,
+        quantidadeProdutos,
+        setQuantidadeProdutos
+        }}
+      >
       {children}
     </CarrinhoContext.Provider>
   )
 };
 
 export const useCarrinhoContext = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+  const {
+    carrinho, 
+    setCarrinho,
+    quantidadeProdutos,
+    setQuantidadeProdutos
+  } = useContext(CarrinhoContext);
 
   function mudarQuantidade(id, quantidade) {
     return carrinho.map(itemDoCarrinho => {
@@ -42,10 +55,16 @@ export const useCarrinhoContext = () => {
     setCarrinho(mudarQuantidade(id, -1));
   }
 
+  useEffect(() => {
+    const novaQuantidade = carrinho.reduce((contador, produto) => contador + produto.quantidade, 0);
+    setQuantidadeProdutos(novaQuantidade);
+  }, [carrinho, setQuantidadeProdutos]);
+
   return {
     carrinho,
     setCarrinho,
     adicionarProduto,
-    removerProduto
+    removerProduto,
+    quantidadeProdutos
   }
 }
