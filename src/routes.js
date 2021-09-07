4c3ffad9a5ed3b548b9d35ff928c495da05b6f03
Luiz Fernando <lfrprazeres@gmail.com>
2021-09-07 10:57:45 -0300

    aula4.4 - Abrindo a tela de carrinho

--- src/routes.js
+++ src/routes.js
@@ -17,11 +17,11 @@ function Router() {
             <Route path="/feira">
               <Feira />
             </Route>
-        </CarrinhoProvider>
+            <Route path="/carrinho">
+              <Carrinho />
+            </Route>
+          </CarrinhoProvider>
         </UsuarioProvider>
-        <Route path="/carrinho">
-          <Carrinho />
-        </Route>
       </Switch>
     </BrowserRouter>
   )
